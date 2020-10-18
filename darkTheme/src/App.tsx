f3a20fcde27f0e33b9cfa074947885830c2f5c83
Ariane Sousa <ariane.sousa011@gmail.com>
2020-10-18 16:55:02 -0300

    Thema dark GitHub

---
+++ darkTheme/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import { DefaultTheme, ThemeProvider } from  'styled-components';
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+import usePersistedState from './utils/usePersistedState';
+
+const App = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
